a07d1a2118336b97f48a14b7dc74af35091dbe8c
unknown <vu.pham@PC00481.infonam.com>
2022-06-10 15:12:05 +0700

    up

--- src/dbcontrollers/alert_data.controller.js
+++ src/dbcontrollers/alert_data.controller.js
@@ -5,10 +5,21 @@ const ALERT_DATA = models.alert_data
 
 async function db_get_alert_data(params) {
     try {
+        let limit = params.limit
+        let offset = (params.offset - 1) * limit
+        let condition = {
+            pid: params.pid
+        }
+        if (params.search) {
+            offset = 0
+            params.search = (params.search).toLowerCase()
+            condition = {
+                pid: params.pid,
+                device_type: { [Op.like]: `%${params.search}%` }
+            }
+        }
         return await ALERT_DATA.findAll({
-            where: {
-                pid: params.pid
-            },
+            where: condition,
             order: [["time", "DESC"]]
         })
     } catch (error) {
