e9e7fa5c7a2bb8f04122591e1d9266dfc43954c0
unknown <vu.pham@PC00481.infonam.com>
2022-06-14 16:27:27 +0700

    added get logger list, download, upload file

--- src/dbmodels/sequelizeEMRModels/logger_data.js
+++ src/dbmodels/sequelizeEMRModels/logger_data.js
@@ -16,7 +16,7 @@ module.exports = function (sequelize, DataTypes) {
                 defaultValue: Sequelize.fn("now"),
             },
             url: {
-                type: DataTypes.STRING(45),
+                type: DataTypes.STRING(255),
                 allowNull: false,
             }
         },
