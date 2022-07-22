738bb91858436d84f087c55848412c1a6a156e1e
vuphamterra <vu.pham@terralogic.com>
2022-07-22 10:10:17 +0700

    Added filter medication

--- src/dbcontrollers/medication.js
+++ src/dbcontrollers/medication.js
@@ -13,11 +13,18 @@ const Medication = models.medication
 async function db_get_medication_list(params) {
     const t = await sequelizeDB.transaction()
     try {
+        let condition = {
+            pid: params.pid
+        }
+
+        if(params.date){
+            condition.end_date = {
+                [Op.gte]: new Date(moment(params.date).format('YYYY-MM-DD'))
+            }
+        }
         const data = await Medication.findAll({
             attributes: ['id','drug_name', 'type', 'occurrence', 'morning', 'afternoon', 'evening', 'night', 'dosage_morning', 'dosage_afternoon', 'dosage_evening', 'dosage_night', 'start_date', 'end_date'],
-            where: {
-                pid: params.pid
-            }
+            where: condition
         },
             { transaction: t })
         const result = { data: data }
