980714491c109485c68ea46cc8053eb334da86e3
vuphamterra <vu.pham@terralogic.com>
2022-07-22 09:24:43 +0700

    Added night time status

--- src/dbcontrollers/medication.js
+++ src/dbcontrollers/medication.js
@@ -14,7 +14,7 @@ async function db_get_medication_list(params) {
     const t = await sequelizeDB.transaction()
     try {
         const data = await Medication.findAll({
-            attributes: ['id','drug_name', 'type', 'occurrence', 'morning', 'afternoon', 'evening', 'dosage_morning', 'dosage_afternoon', 'dosage_evening', 'start_date', 'end_date'],
+            attributes: ['id','drug_name', 'type', 'occurrence', 'morning', 'afternoon', 'evening', 'night', 'dosage_morning', 'dosage_afternoon', 'dosage_evening', 'dosage_night', 'start_date', 'end_date'],
             where: {
                 pid: params.pid
             }
